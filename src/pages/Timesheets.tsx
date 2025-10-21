4405c0179ceb18943f44593c62e6049e4582febb
Urnemsis <asmariqbal13@gmail.com>
2025-10-21 00:00:57 -0700

    Create Timesheets.tsx

---
+++ src/pages/Timesheets.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Timesheets: React.FC = () => {
+  return (
+    <div>
+      <h1>Timesheets</h1>
+      {/* TODO: Add timesheets management */}
+    </div>
+  );
+};
+
+export default Timesheets;
