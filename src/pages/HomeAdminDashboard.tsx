e71372e9b286133e0d41022a23b8b7dcdc63962b
Urnemsis <asmariqbal13@gmail.com>
2025-10-20 23:53:27 -0700

    Create HomeAdminDashboard.tsx

---
+++ src/pages/HomeAdminDashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomeAdminDashboard: React.FC = () => {
+  return (
+    <div>
+      <h1>Home Admin Dashboard</h1>
+      {/* TODO: Add home admin dashboard content */}
+    </div>
+  );
+};
+
+export default HomeAdminDashboard;
