5bcad9aa2fb7a8fb65ddde6b181d601512aa30e0
Urnemsis <asmariqbal13@gmail.com>
2025-10-20 23:37:31 -0700

    Create SignUp.tsx

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SignUp: React.FC = () => {
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      {/* TODO: Add sign-up form */}
+    </div>
+  );
+};
+
+export default SignUp;
