53abee832fff25d1b053dddf6c74656bbc941489
Urnemsis <asmariqbal13@gmail.com>
2025-10-20 23:42:29 -0700

    Create Login.tsx

---
+++ src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login: React.FC = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+      {/* TODO: Add login form */}
+    </div>
+  );
+};
+
+export default Login;
