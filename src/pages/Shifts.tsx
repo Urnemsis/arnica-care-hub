cb9704d8b1db5993f113ca157ebff3f5fc900564
Urnemsis <asmariqbal13@gmail.com>
2025-10-20 23:55:25 -0700

    Create Shifts.tsx

---
+++ src/pages/Shifts.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Shifts: React.FC = () => {
+  return (
+    <div>
+      <h1>Shifts</h1>
+      {/* TODO: Add shift listing */}
+    </div>
+  );
+};
+
+export default Shifts;
