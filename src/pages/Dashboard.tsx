cbcbf92e97ae73048abf5e2becfd57de8726a4a4
Urnemsis <asmariqbal13@gmail.com>
2025-10-20 23:46:54 -0700

    Create Dashboard.tsx

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      {/* TODO: Add dashboard content */}
+    </div>
+  );
+};
+
+export default Dashboard;
