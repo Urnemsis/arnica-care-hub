7d601a317f0f499b7814da55753f5a96465d813d
Urnemsis <asmariqbal13@gmail.com>
2025-10-20 23:58:03 -0700

    Create ShiftDetails.tsx

---
+++ src/pages/ShiftDetails.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ShiftDetails: React.FC = () => {
+  return (
+    <div>
+      <h1>Shift Details</h1>
+      {/* TODO: Add shift details content */}
+    </div>
+  );
+};
+
+export default ShiftDetails;
